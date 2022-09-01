032fe2391903ad81185334ef0cdfee203141b0cb
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2022-09-01 19:51:48 +0300

    feat: create basic vue2 App
    Add simple components

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <BtnR>Click me</BtnR>
+    <Title></Title>
+  </div>
+</template>
+
+<script>
+import BtnR from './components/Button';
+import Title from './components/TitleEX.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BtnR,
+    Title,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Button1.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+const Button1 = Vue.component('BtnT', {
+  template: '<button>Click me one more time</button>',
+});
+
+export default Button1;
