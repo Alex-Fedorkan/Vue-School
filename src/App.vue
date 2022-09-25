d5fc10bfcc68dee82cb3efb9f0f5a1b16d2b81c2
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2022-09-25 15:31:08 +0300

    feat: add simple components

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :id="$style.app">
+    <h1>{{ title }}</h1>
+    <BasicButton @click="increment" type="button" outlined
+      >Click me</BasicButton
+    >
+    <DirectivesTest />
+  </div>
+</template>
+
+<script>
+import BasicButton from './components/BasicButton';
+import DirectivesTest from './components/DirectivesTest.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BasicButton,
+    DirectivesTest,
+  },
+  data() {
+    return { amountOfClicks: 0 };
+  },
+  computed: {
+    title() {
+      return `Amount of clicks ${this.amountOfClicks}`;
+    },
+  },
+  methods: {
+    increment() {
+      this.amountOfClicks += 1;
+    },
+  },
+};
+</script>
+
+<style module>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BasicButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <button
+    v-on="$listeners"
+    v-bind:type="type"
+    :class="{ btn: true, 'btn--outlined': outlined }"
+    style="opacity: 0.5"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BasicButton',
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  padding: 8px 15px;
+  min-width: 220px;
+  display: inline-block;
+
+  font-size: 18px;
+  color: #fff;
+
+  background-color: #ff662d;
+  border: 1px solid transparent;
+
+  cursor: pointer;
+
+  &--outlined {
+    color: #ff662d;
+
+    background: none;
+    border: 1px solid #ff662d;
+  }
+}
+</style>
