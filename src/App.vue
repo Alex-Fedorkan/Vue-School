a12b502a973660945462c2d92ac882adcc86c987
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2022-09-25 16:33:01 +0300

    feat: lesson 8, add StarRating component

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :id="$style.app">
+    <h1>{{ title }}</h1>
+    <StarRating :rating="rating" />
+  </div>
+</template>
+
+<script>
+import StarRating from './components/StarRating.vue';
+
+export default {
+  name: 'App',
+  components: {
+    StarRating,
+  },
+  data() {
+    return {
+      amountOfClicks: 0,
+      rating: 4.5,
+    };
+  },
+  computed: {
+    title() {
+      return `Amount of clicks ${this.amountOfClicks}`;
+    },
+  },
+  methods: {
+    increment() {
+      this.amountOfClicks += 1;
+    },
+  },
+};
+</script>
+
+<style module>
+#app {
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DirectivesTest.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="isVisible">
+    <h1 v-show="false">Title Directives</h1>
+    >
+    <ul>
+      <template v-for="fruit in fruits">
+        <li v-if="fruit === 'banana'" :key="fruit">{{ fruit }}</li>
+      </template>
+    </ul>
+    <ul>
+      <li v-for="(car, key) in cars" :key="car.color">
+        {{ car.color }} {{ key }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DirectivesTest',
+  data() {
+    return {
+      isVisible: true,
+      fruits: ['kiwi', 'banana', 'apple'],
+      cars: {
+        audi: { color: 'blue' },
+        bmw: { color: 'yellow' },
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
