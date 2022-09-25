b0e713a0b4f2db14471fb6592ef534f1c3bedb7e
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2022-09-25 21:33:16 +0300

    feat: lesson 9, add ApartmentsItem component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 <template>
   <div :id="$style.app">
-    <h1>{{ title }}</h1>
-    <StarRating :rating="rating" />
+    <ApartmentsItem
+      :descr="apartment.descr"
+      :price="apartment.price"
+      :rating="apartment.rating"
+      imgSrc="https://cdn.mos.cms.futurecdn.net/CAZ6JXi6huSuN4QGE627NR-1200-80.jpg.webp"
+    />
   </div>
 </template>
 
 <script>
-import StarRating from './components/StarRating.vue';
+import ApartmentsItem from './components/Apartment/ApartmentsItem.vue';
 
 export default {
   name: 'App',
-  components: {
-    StarRating,
-  },
+  components: { ApartmentsItem },
   data() {
     return {
-      amountOfClicks: 0,
-      rating: 4.5,
+      apartment: {
+        id: '5f05c9dad2c9bc0f773444bc',
+        title:
+          'Aut qui adipisci distinctio maiores molestiae sit est inventore vero.',
+        descr:
+          'Non perferendis rerum a in nisi exercitationem dolorum perferendis. Eligendi sit error sed a . Facere maiores sit adipisci sequi eveniet. Qui est voluptatum maiores eos qui vitae.',
+        price: 2032,
+        rating: 4.4,
+        location: { city: 'Kherson' },
+        owner: {
+          name: 'Ellen',
+          phone: ' 115-355-5652',
+          email: 'Tracey.Morar86@hotmail.com',
+        },
+      },
     };
-  },
-  computed: {
-    title() {
-      return `Amount of clicks ${this.amountOfClicks}`;
-    },
-  },
-  methods: {
-    increment() {
-      this.amountOfClicks += 1;
-    },
   },
 };
 </script>
---
+++ src/components/Apartment/ApartmentsItem.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="apartments-item">
+    <div class="apartments-item__inner">
+      <img class="apartments-item__photo" :src="imgSrc" alt="" />
+      <div class="apartments-item__content">
+        <p class="apartments-item__description">{{ descr }}</p>
+        <div class="apartments-item__rating">
+          <StarRating :rating="rating" />
+        </div>
+        <div class="apartments-item__price">UAH {{ price }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import StarRating from '../StarRating.vue';
+
+export default {
+  name: 'ApartmentsItem',
+  components: { StarRating },
+  props: {
+    descr: {
+      type: String,
+      default: '',
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    price: { type: Number, required: true },
+    imgSrc: { type: String, default: '' },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.apartments-item {
+  position: relative;
+
+  padding: 0 20px;
+  max-width: 300px;
+
+  &__inner {
+    position: relative;
+  }
+
+  &__content {
+    position: relative;
+
+    padding: 20px;
+    min-height: 200px;
+
+    line-height: 1.4;
+    text-align: start;
+    color: #fff;
+
+    background: rgba(#0f1d2d, 0.7);
+
+    opacity: 0;
+
+    transition: opacity 0.4s;
+
+    z-index: 1;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  &__description {
+    margin-bottom: 20px;
+  }
+
+  &__rating {
+    margin-bottom: 20px;
+  }
+
+  &__price {
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  &__photo {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    object-fit: cover;
+  }
+}
+</style>
